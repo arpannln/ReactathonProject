cd8fe45016fbf42e1e2eb3286090e8154e81216e
chao <mailbox.chao.cao@gmail.com>
2018-03-24 16:48:55 -0700

    Add splash page and styling

---
+++ splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Splash extends React.Component{
+  constructor(props){
+    super(props);
+  }
+}
+
+render () {
+  <div>
+
+  </div>
+}
