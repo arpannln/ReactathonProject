c603c273ebce11e8f3c798f8c9665f98f37bc41b
Edward <edwardcheng148@gmail.com>
2018-03-25 09:51:42 -0700

    added span outer layer

--- microservices/ui/app/src/components/rooms.jsx
+++ microservices/ui/app/src/components/rooms.jsx
@@ -46,15 +46,17 @@ class Rooms extends Component {
         this.state.rooms.map(room => {
           console.log(room.sessionId);
           return (
-            <Link
-              to={`/rooms/${room.session_id}`}
-              key={`${room.id}`}
-              className="link">
-              <div
-              className="topic-list-item">
-                <h3>{room.name}</h3>
-              </div>
-            </Link>
+            <span className="span-button">
+              <Link
+                to={`/rooms/${room.session_id}`}
+                key={`${room.id}`}
+                className="link">
+                <div
+                className="topic-list-item">
+                  <h3>{room.name}</h3>
+                </div>
+              </Link>
+            </span>
           )
         })
       }
